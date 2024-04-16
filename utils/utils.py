e7956c657bf714fce9c88f628ef6144aa563431e
Ksenia-Kostyuk <hatan2235@mail.ru>
2024-04-16 12:46:57 +0300

    code database

---
+++ utils/utils.py
@@ -0,0 +1,21 @@
+#Коды для создания таблиц
+#-- таблица с вакансиями
+#CREATE TABLE vacancies
+#(
+#	vacancy_id int PRIMARY KEY,
+#	company_id int,
+#	vacancy_name varchar(150),
+#	salary_from int,
+#	salary_to int,
+#	url_vacancy text,
+#	responsibility text
+#)
+#-- таблица с компаниями
+#CREATE TABLE company
+#(
+#	company_id int PRIMARY KEY,
+#	name_company varchar(150),
+#	vacancies int
+#)
+
+employer_id = [10341776, 10537291, 9489877, 9066698, 78638, 2748, 3529, 1947314, 60377, 9498112]
